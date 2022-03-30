6f9de5ee76f7d30d49f8b531c941efbcd194938f
You Garmendia <you.garmendia.ayala@ciisa.cl>
2022-03-30 11:23:46 -0300

    Por fin entendí el rendereo de hsb a HTML

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require('express') // Infraestructura web para node
+const morgan = require('morgan') // Middleware, es un logger
+const path = require('path') // Esto me permite poner las direcciones.
 
-const PORT = process.env.PORT || 3000;
-const app = express();
+const app = express() // Toda la infraestructura ahora pasará a llamarse app
+const PORT = process.env.PORT || 3000
+// Acá configuramos nuestro sistema MVC
+app.set('views', path.join(__dirname, 'views')) // Indicamos que nuestra sección VISTA será el directorio Vista del directorio en el que estoy trabajando
+app.set('view engine', 'hbs') // Acá indico que mi view engine sea hbs ¿o sea lo que se transformará a HTML?
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-  res.send('Hola a todos, mi nombre es You Garmendia');
-});
+  res.render('home')
+})
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`)
 })
