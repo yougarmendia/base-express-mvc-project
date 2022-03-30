8d0c70c31ac50d4d53b3813f838130b08c5d7d61
You Garmendia <you.garmendia.ayala@ciisa.cl>
2022-03-30 16:12:03 -0300

    solucionado lo de la página about

--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ const pageController = new PageController()
 // Routes
 router.get('/', pageController.renderHome)
 
-router.get('about', pageController.renderAbout)
+router.get('/about', pageController.renderAbout)
 
 router.get('*', pageController.renderNotFound) // Éste SIEMPRE tiene que ir al final, porque es el que define qué se entrega al usuario si pone alguna URL que no tenemos.
 
